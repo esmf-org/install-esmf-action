b8a335300bf2c584f584c525b29e7ef4ad1cdadc
Daniel Rosen <drosen@ucar.edu>
2025-09-22 14:29:10 -0600

    Fix esmpy installation (#20)
    * Cleanup esmpy installation
    * Fix ESMX test

--- __tests__/Cap/cap.F90
+++ __tests__/Cap/cap.F90
@@ -182,7 +182,8 @@ module CAP
       file=__FILE__)) &
       return  ! bail out
 
-    call ESMF_VMLog(vm, "CAP Advance(): ", ESMF_LOGMSG_INFO, rc=rc)
+    call ESMF_VMLog(vm, prefix="CAP Advance(): ", &
+      logMsgFlag=ESMF_LOGMSG_INFO, rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
       line=__LINE__, &
       file=__FILE__)) &
